fa382e90b9954c2754bc469aa59bd2f9367552ce
kickinbahk <josiahmory@gmail.com>
2016-02-20 12:50:46 -0800

    Refactor components add flux

---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+    site: {
+        title: 'React Universal Blog'
+    },
+    bucket: {
+        slug: 'react-universal-blog',
+        media_url: 'https://cosmicjs.com/uploads'
+    }
+}
+
