e02be6166e63c717e160297170900f31179b880d
kickinbahk <josiahmory@gmail.com>
2016-02-20 11:11:56 -0800

    add routes

---
+++ app-client.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router } from 'react-router'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+const history = createBrowserHistory()
+
+// Routes
+import routes from './routes'
+
+const Routes = (
+        <Router history = {history}>
+          { routes }
+        </Router>
+)
+
+const app = document.getElementbyId('app')
+render(Routes, app)
+
+
+
